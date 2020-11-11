1166adb7773091121d5daa5c6299feb97e85e3aa
sanjaylokula <lokula.s@northeastern.edu>
2020-11-11 18:11:41 -0500

    Added Training and webcam files

---
+++ Classifier/python/webcam.py
@@ -0,0 +1,75 @@
+import os
+import zipfile
+import random
+import shutil
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from shutil import copyfile
+from os import getcwd
+from os import listdir
+import cv2
+from tensorflow import keras
+from tensorflow.keras.layers import Conv2D, Input, ZeroPadding2D, BatchNormalization, Activation, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.utils import shuffle
+import imutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image  as mpimg
+
+
+model = keras.models.load_model()
+face_clsfr=cv2.CascadeClassifier("../inputfiles/haarcascade_frontalface_default.xml")
+
+labels_dict = {0: 'without_mask', 1: 'with_mask'}
+color_dict = {0: (0, 0, 255), 1: (0, 255, 0)}
+
+size = 4
+webcam = cv2.VideoCapture(0)  # Use camera 0
+
+# We load the xml file
+classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    (rval, im) = webcam.read()
+    im = cv2.flip(im, 1, 1)  # Flip to act as a mirror
+
+    # Resize the image to speed up detection
+    mini = cv2.resize(im, (im.shape[1] // size, im.shape[0] // size))
+
+    # detect MultiScale / faces
+    faces = classifier.detectMultiScale(mini)
+
+    # Draw rectangles around each face
+    for f in faces:
+        (x, y, w, h) = [v * size for v in f]  # Scale the shapesize backup
+        # Save just the rectangle faces in SubRecFaces
+        face_img = im[y:y + h, x:x + w]
+        resized = cv2.resize(face_img, (150, 150))
+        normalized = resized / 255.0
+        reshaped = np.reshape(normalized, (1, 150, 150, 3))
+        reshaped = np.vstack([reshaped])
+        result = model.predict(reshaped)
+        # print(result)
+
+        label = np.argmax(result, axis=1)[0]
+
+        cv2.rectangle(im, (x, y), (x + w, y + h), color_dict[label], 2)
+        cv2.rectangle(im, (x, y - 40), (x + w, y), color_dict[label], -1)
+        cv2.putText(im, labels_dict[label], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+
+    # Show the image
+    cv2.imshow('LIVE', im)
+    key = cv2.waitKey(10)
+    # if Esc key is press then break out of the loop
+    if key == 27:  # The Esc key
+        break
+# Stop video
+webcam.release()
+
+# Close all started windows
+cv2.destroyAllWindows()
