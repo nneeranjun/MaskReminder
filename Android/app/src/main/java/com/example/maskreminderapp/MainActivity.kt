672f068feb73119b06ebf36d361835d4b357beae
Nilay Neeranjun <nneeranjun@berkeley.edu>
2021-01-02 10:54:17 -0500

    Finished app:

--- Android/app/src/main/java/com/example/maskreminderapp/MainActivity.kt
+++ Android/app/src/main/java/com/example/maskreminderapp/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.maskreminderapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.maps.*
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG: String? = MainActivity::class.simpleName
+    }
+
+    @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            //val msg = getString(R.string.msg_token_fmt, token)
+            Log.d(TAG, token)
+            print(token)
+            Toast.makeText(baseContext, token, Toast.LENGTH_LONG).show()
+        })
     }
+    
 }
